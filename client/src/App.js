b430cdc2898930b82b857062287a97221c5fea03
Daniel Coulson <danycoulson@hotmail.com>
2021-01-27 18:31:57 -0800

    react navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import "./styles/app.css";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <p>Logo</p>
+      <p>Services</p>
+      <p>About us</p>
+      <p>Contact Us</p>
+    </div>
+  );
+}
