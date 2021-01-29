ffdc4653b845b10ede5a2c6bab7148c6359c7255
Daniel Coulson <danycoulson@hotmail.com>
2021-01-28 19:50:36 -0800

    Server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Your first Request");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost/${PORT}`);
+});
