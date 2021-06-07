90798eb39ebafb756e52955e06636f116581eaf7
Daniel Coulson <danycoulson@hotmail.com>
2021-06-07 15:47:51 -0700

    more styling

--- client/src/components/DescInfo.jsx
+++ client/src/components/DescInfo.jsx
@@ -3,11 +3,12 @@ import React from "react";
 export default function DescInfo(props) {
   let { title, location, date, description } = props.info;
   return (
-    <div>
-      <h2>Title</h2> <p>{title}</p>
-      <h2>Location</h2> <p>{location}</p>
-      <h2>Date</h2> <p>{date}</p>
-      <h2>Description</h2> <p>{description}</p>
+    <div className="desc-info">
+      <h2 className="desc-info__title">Title</h2> <p>{title}</p>
+      <h2 className="desc-info__location">Location</h2> <p>{location}</p>
+      <h2 className="desc-info__date">Date</h2> <p>{date}</p>
+      <h2 className="desc-info__description">Description</h2>
+      <p>{description}</p>
     </div>
   );
 }
